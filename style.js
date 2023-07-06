ec5e4d56c6b05405bf9df5d503b465916b8c7c52
Layola Mbula <94037423+Layola17@users.noreply.github.com>
2023-07-06 04:47:02 -0700

    Update style.js

--- style.js
+++ style.js
@@ -1,19 +1,40 @@
-// Get the button
-let mybutton = document.getElementById("myBtn");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
 }
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+//This is to submit the form to my google sheet
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzY8hW3xcesGRZ-VStkcV-xSqkv3JYjql68BtqcHHdeqEhDyABzhHWKMarhLtwXQcc64Q/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
